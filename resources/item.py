b82eac059155313052ac046f4848fe4406bed37c
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-05 19:14:58 -0300

    marshmallow schemas are now correctly inserted in project with a more clear code and better documentation

--- resources/item.py
+++ resources/item.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import items
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations on items")
 
@@ -22,13 +23,8 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
-            )
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
 
@@ -44,18 +40,11 @@ class Item(MethodView):
 class ItemList(MethodView):
     def get(self):
         return {"items": list(items.values())}
-
-    def post(self):
-        item_data = request.get_json()
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-            )
+    
+    # ItemSchema não está apenas validando o tipo de dado recebido, mas tambem fazendo:
+    # item_data = request.get_json(), ou seja um "auto-fetch"
+    @blp.arguments(ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
