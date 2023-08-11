f5a65cc31de1e933d855dba5b800e579627cd671
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-11 18:53:27 -0300

    python are now working with redis-docker and correctly revoking tokens

--- blocklist.py
+++ blocklist.py
@@ -10,5 +10,18 @@ todos os tokens revogados são aceitos novamente, assim um usuário deslogado,
 poderia realizar ações normalmente.
 Para resolver isso use um Redis(db de memória) para salvar o blocklist.
 """
+import redis
+from datetime import timedelta
 
-BLOCKLIST = set()
+ACCESS_EXPIRES = timedelta(hours=1) # expira em 60 minutos
+  
+# -----------Redis proxy config---------------------------
+jwt_redis_blocklist = redis.StrictRedis(
+  host="localhost", port=6379, db=0, decode_responses=True
+)
+# -----------Redis proxy config---------------------------
+# PING test
+value = jwt_redis_blocklist.set("key33", "value33")
+print(value)
+
+# BLOCKLIST = set()
