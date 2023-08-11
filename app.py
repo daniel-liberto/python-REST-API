d1b16c5ee59aca921b8f21a3619b1955fbd0beea
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-11 15:53:42 -0300

    jwt required imported it

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ então no front end é só puxar store.items[item_id].message.
 import os
 import secrets
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
@@ -41,6 +41,25 @@ def create_app(db_url=None):
   
   app.config["JWT_SECRET_KEY"] = "227908941795316218633443429225171957379"
   jwt = JWTManager(app)
+  
+  # optional extra info para o jwt em todas as vezes que um token é gerado
+  @jwt.additional_claims_loader
+  def add_claims_to_jwt(identity): # identity(user.id) = vem do user.py(resource)/access_token(linha 35)
+    if identity == 1:
+      return {"is_admin": True}
+    return {"is_admin": False}
+  
+  @jwt.expired_token_loader
+  def expired_token_callback(jwt_header, jwt_payload):
+    return (jsonify({"message": "The token has expired.", "error": "token_expired"}), 401)
+
+  @jwt.invalid_token_loader
+  def invalid_token_callback(error):
+    return (jsonify({"message": "Signature verification failed.", "error": "invalid_token"}), 401)
+  
+  @jwt.unauthorized_loader
+  def missing_token_callback(error):
+    return(jsonify({"description": "Request does not contain an access token.", "error": "authorization_required"}), 401)
 
   with app.app_context(): 
     db.create_all() # este comando vai criar todas as tabelas no banco
--- resources/item.py
+++ resources/item.py
@@ -1,7 +1,6 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from flask_jwt_extended import jwt_required, get_jwt
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
@@ -13,12 +12,19 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 
 @blp.route("/item/<int:item_id>")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id) #get or 404 é auto explicativo (flask-sqlalchemy)
         return item
 
+    @jwt_required()
     def delete(self, item_id):
+        # ----------optional jwt claims verification --------
+        jwt = get_jwt()
+        if not jwt.get("is_admin"):
+            abort(401, message="Admin privilege required.")
+        # ----------optional jwt claims verification --------
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
         db.session.commit()
@@ -44,12 +50,14 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True)) # many = true, cria uma lista de itens.
     def get(self):
         return ItemModel.query.all()
     
     # ItemSchema não está apenas validando o tipo de dado recebido, mas tambem fazendo:
     # item_data = request.get_json(), ou seja um "auto-fetch"
+    @jwt_required() # post requer um token jwt
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
