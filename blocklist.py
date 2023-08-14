6b2f75aff577d43f6de18f06bea8ed66717d968b
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-14 14:38:10 -0300

    some minor changes to redis db

--- blocklist.py
+++ blocklist.py
@@ -5,7 +5,13 @@ ACCESS_EXPIRES = timedelta(hours=1)
 REFRESH_EXPIRES = timedelta(days=3)
 
 # -----------Redis proxy config---------------------------
+# jwt_redis_blocklist = redis.StrictRedis(
+#   host="localhost", port=6379, db=0, decode_responses=True
+# )
+# -----------Redis proxy config---------------------------
+
+# -----------Redis proxy config---------------------------
 jwt_redis_blocklist = redis.StrictRedis(
-  host="localhost", port=6379, db=0, decode_responses=True
+  host="redis://red-cjd6c745kgrc73atcu2g", port=6379, db=0, decode_responses=True
 )
 # -----------Redis proxy config---------------------------
