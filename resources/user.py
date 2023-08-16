0f768328154b4877390e5442ae0fa44ed3919e26
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-16 16:44:58 -0300

    minor change

--- resources/user.py
+++ resources/user.py
@@ -35,7 +35,8 @@ class UserLogin(MethodView):
     if user and pbkdf2_sha256.verify(user_data["password"], user.password):
       access_token = create_access_token(identity=user.id, fresh=True)
       refresh_token = create_refresh_token(identity=user.id)
-      return {"access_token": access_token, "refresh_token": refresh_token,"message": "Successfully logged in."}
+      user_id = user_data["id"]
+      return {"user_id": user_id, "access_token": access_token, "refresh_token": refresh_token,"message": "Successfully logged in."}
     abort(401, message="Invalid credentials.")
 
 @blp.route("/refresh")
